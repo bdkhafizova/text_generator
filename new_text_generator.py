5b72a1a1db3a2ab4006ab1590b09275540ea0edb
Vitaliy Tomilov <vtomilov@progresspoint.ru>
2015-12-04 23:38:54 +0300

    Corrected style

--- new_text_generator.py
+++ new_text_generator.py
@@ -8,53 +8,62 @@ import cPickle as pickle
 from scipy import stats
 
 
-dirs = ['dickens']
-
 def create_dict():
     return defaultdict(float)
 
+
 class Generator:
 
-    def __init__(self, path):             
+    def __init__(self, path, new_db_indicator):
+        self.paths = path
+        if (new_db_indicator):
+            self._create_stats()
+        else:
+            self._load_stats()
         self.word_pattern =\
-                re.compile(ur'[^\s!,?\-":.;()\u201c\u201d\u201e\u2018*0-9]+', 
-                           re.UNICODE)
+            re.compile(ur'[^\s!,?\-":.;()\u201c\u201d\u201e\u2018*0-9]+',
+                       re.UNICODE)
         self.quotes_pattern =\
-                re.compile(ur'["\u2019\u2018\u201d\u201e\u201c]?',
-                           re.UNICODE)
+            re.compile(ur'["\u2019\u2018\u201d\u201e\u201c]?',
+                       re.UNICODE)
         self.delim_pattern = re.compile(ur'[.!?,:;-]?', re.UNICODE)
         self.cache = deque()
         self.stats = defaultdict(create_dict)
         self.space = 0.0
         self.enter = 0.0
-        self.quote = 0.0
-        self.non_quote = 0.0
         self.total = 0.0
         self.counter = 0.0
         self.quote_state = False
         self.sentence_end = True
         self.recursion_depth = 0
         self.real_delims = [u'.', u'!', u'?']
-        self.delims = [u'.', u'!', u'?', u',', u':']
-        self.quotes = [u'\'', u'"', u'\u2019', u'\u2018', u'\u201d', u'\u201e', u'\u201c']
-        self.quotes_observer = {u'"': False, u'\'': False} 
+        self.delims = [u'.', u'!', u'?', u',', u':', u';']
+        self.quotes = [u'\'', u'"', u'\u2019', u'\u2018', u'\u201d', u'\u201e',
+                       u'\u201c']
+        self.quotes_observer = {u'"': False, u'\'': False}
 
+    def _create_stats(self):
+        self._process_text()
+        self._calculate_params()
+        self._normalize_stats()
+        self._dump_stats()
 
-    def words(self, fileobj):
+    def _words(self, fileobj):
         for line in fileobj:
             if len(line) > 1:
                 self.enter += 1
             for word in line.split():
                 yield word
 
-    def process_word(self, word):
+    def _process_word(self, word):
         pure_word = [w for w in self.word_pattern.findall(word) if w != '']
         if not pure_word:
             return list()
         quotes = [q for q in self.quotes_pattern.findall(word) if q != '']
         delims = [d for d in self.delim_pattern.findall(word) if d != '']
         final_words = list()
-        if self.sentence_end and (pure_word[0][0].isupper() or len(quotes) != 0):
+        if self.sentence_end and\
+           (pure_word[0][0].isupper() or len(quotes) != 0):
             final_words.append('')
             self.sentence_end = False
         final_words.append(pure_word[0])
@@ -65,23 +74,22 @@ class Generator:
             final_words.append(delim)
         for quote in quotes:
             if -1 < word.find(quote) < word.find(pure_word[0]):
-                final_words.insert(1, quote if not quote in [u'\'', u'"'] else u'\u201c')
+                final_words.insert(1, quote if quote not in [u'\'', u'"']
+                                   else u'\u201c')
             elif word.find(quote) > (word.find(pure_word[0]) + len(pure_word)):
-                    final_words.append(quote if not quote in [u'\'', u'"'] else u'\u201d')
+                final_words.append(quote if quote not in [u'\'', u'"']
+                                   else u'\u201d')
         return final_words
 
-    def push_to_stats(self, word):
+    def _push_to_stats(self, word):
         if len(self.cache) < 3:
-            try:
-                self.cache.append(word)
-                if len(self.cache) == 1:
-                    pass
-                elif len(self.cache) == 2:
-                    self.stats[self.cache[0]][self.cache[1]] += 1
-                else:
-                    self.stats[self.cache[1]][self.cache[2]] += 1
-            except Exception as e:
-                print self.cache
+            self.cache.append(word)
+            if len(self.cache) == 1:
+                pass
+            elif len(self.cache) == 2:
+                self.stats[self.cache[0]][self.cache[1]] += 1
+            else:
+                self.stats[self.cache[1]][self.cache[2]] += 1
         else:
             self.cache.popleft()
             self.cache.append(word)
@@ -90,54 +98,35 @@ class Generator:
             else:
                 self.stats[(self.cache[0], self.cache[1])][self.cache[2]] += 1
 
-
-    def process_text(self):
-        for directory in dirs:     
+    def _process_text(self):
+        for directory in self.paths:
             for book in os.listdir(directory):
-                with codecs.open(directory + '/' + book, encoding='utf8') as wordfile:
-                    wordgen = self.words(wordfile)
-                    for raw_word in wordgen:
-                        final_words = self.process_word(raw_word)
-                        for word in final_words:
-                            self.push_to_stats(word)
-                self.push_to_stats('')
+                with codecs.open(directory + '/' + book, encoding='utf8')\
+                     as wordfile:
+                    for raw_word in self._words(wordfile):
+                        for word in self._process_word(raw_word):
+                            self._push_to_stats(word)
+                self._push_to_stats('')
                 self.cache.clear()
-            print self.enter
-            print self.total
-            self.space = self.total - self.enter
-            self.space /= self.total
-            self.enter /= self.total
-            self.non_quote = self.total - self.quote
-            self.non_quote /= self.total
-            self.quote /= self.total
-            #self.dump_stats(directory)
-            #with codecs.open('stats_' + book, 'w', encoding='utf8') as statfile:
-            #    for stat in self.stats.items():
-            #        key_stat, enum_stat = stat
-            #        if isinstance(key_stat, tuple):
-            #            statfile.write(u', '.join(item for item in key_stat) + '\n')
-            #        for values in enum_stat.items():
-            #            ww, cc = values
-            #            statfile.write(ww + ' ' + str(cc) + '\n')
+
+    def _calculate_params(self):
+        self.space = self.total - self.enter
+        self.space /= self.total
+        self.enter /= self.total
+
+    def _normalize_stats(self):
         for beginning in self.stats:
-            norm_factor = sum(self.stats[beginning][rest] for rest in self.stats[beginning])
+            norm_factor = sum(self.stats[beginning][rest]
+                              for rest in self.stats[beginning])
             for rest in self.stats[beginning]:
                 self.stats[beginning][rest] /= norm_factor
-        with codecs.open('stats_new', 'w', encoding='utf8') as statfile:
-            for stat in self.stats.items():
-                key_stat, enum_stat = stat
-                if isinstance(key_stat, tuple):
-                    statfile.write(u', '.join(item for item in key_stat) + '\n')
-                for values in enum_stat.items():
-                    ww, cc = values
-                    statfile.write(ww + ' ' + str(cc) + '\n')
 
-    def dump_stats(self):
+    def _dump_stats(self):
         with open('stats', 'wb') as statfile:
             pickle.dump(self.stats, statfile)
-            
-    def load_stats(self):
-        with open('stats', 'rb') as statfile:
+
+    def _load_stats(self):
+        with open(self.paths, 'rb') as statfile:
             self.stats = pickle.load(statfile)
 
     def generate_text(self, number):
@@ -145,48 +134,40 @@ class Generator:
         self.text = str()
         while (len(self.text_list) < number):
             sentence = self.generate_sentence()
-            print sentence
             self.text_list.append(sentence)
-        
-        vals = [0,1]
-        space_or_enter_gen = stats.rv_discrete(name = 'space_or_enter_gen',
-                values = (vals, [self.space, self.enter]))
+        space_or_enter_gen =\
+            stats.rv_discrete(name='space_or_enter_gen',
+                              values=([0, 1], [self.space, self.enter]))
         for sentence in self.text_list:
             self.text += sentence
             delim = space_or_enter_gen.rvs(size=1)
-            self.text += u'\n'.encode('utf-8') if delim[0] else u' '.encode('utf-8')
+            self.text += u'\n'.encode('utf-8') if delim[0]\
+                         else u' '.encode('utf-8')
         print self.text
 
-    def generate_sentence(self):
+    def _generate_sentence(self):
         self.counter += 1
-        print self.counter
         sentence = list()
         sentence.append(self.generate_word(''))
         sentence.append(self.generate_word(sentence[0]))
         while (sentence[-1] != ''):
             sentence.append(self.generate_word((sentence[-2], sentence[-1])))
-        return ' '.join(word.encode('utf-8') for word in sentence[:-1]) + sentence[-1].encode('utf-8')
+        return ' '.join(word.encode('utf-8') for word in sentence[:-1])\
+               + sentence[-1].encode('utf-8')
 
-    def generate_word(self, previous):
+    def _generate_word(self, previous):
         probabilities = list()
         self.recursion_depth += 1
-        #if isinstance(previous, tuple):
-        #    prev1 = unicode()
-        #    prev2 = unicode()
-        #    prev1, prev2 = previous
-        #    print 'Searching for' + prev1.encode('utf-8') + ' ' + prev2.encode('utf-8')
-        #else:
-        #    pass
-        #    print 'Searching for' + previous.encode('utf-8')
         indexes = range(len(self.stats[previous]))
         match_dict = dict()
         for next_word, index in zip(self.stats[previous], indexes):
             probabilities.append(self.stats[previous][next_word])
             match_dict[index] = next_word
         if len(probabilities) == 0:
-            raise RuntimeError('Invalid fields{}'.format(previous.encode('utf-8')))
-        local_generator = stats.rv_discrete(name = 'local_generator', values = (indexes, probabilities))
-        r = local_generator.rvs(size = 1)
+            raise RuntimeError('Invalid fields')
+        local_generator = stats.rv_discrete(name='local_generator',
+                                            values=(indexes, probabilities))
+        r = local_generator.rvs(size=1)
         a = unicode()
         a = match_dict[r[0]]
         if a in self.quotes:
@@ -197,7 +178,6 @@ class Generator:
             else:
                 if self.recursion_depth < 50:
                     a = self.generate_word(previous)
-        #print a.encode('utf-8')
         self.recursion_depth = 0
         return a
 
@@ -205,4 +185,3 @@ class Generator:
 g = Generator('')
 g.process_text()
 g.generate_text(100)
-#g.dump_stats()
